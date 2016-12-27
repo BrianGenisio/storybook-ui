3d668b5f7f5464391c3560016f380f89ac1937db
Brian Genisio <briangenisio@gmail.org>
2016-12-27 16:37:12 -0500

    webpack bundle this as a library

--- .scripts/prepublish.sh
+++ .scripts/prepublish.sh
@@ -10,5 +10,6 @@ echo "=> Transpiling 'src' into ES5 ..."
 echo ""
 rm -rf ./dist
 ./node_modules/.bin/babel --ignore __tests__ --plugins "transform-runtime" ./src --out-dir ./dist
+./node_modules/.bin/webpack ./dist/index.js --output-library-target umd --output-library storybook-ui --output-filename ./dist/storybook-ui.js
 echo ""
 echo "=> Transpiling completed."
